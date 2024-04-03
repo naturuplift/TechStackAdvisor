ab3d7b1e094b3585f3ead00a2dc2432488c71fc7
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-04-03 17:35:13 -0300

    update UI interface

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,19 +15,19 @@ const AppNavbar = () => {
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container fluid>
           <Navbar.Brand as={Link} to='/'>
-            Google Books Search
+            Tech Stack AI Advisor
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
             <Nav className='ml-auto d-flex'>
               <Nav.Link as={Link} to='/'>
-                Search For Books
+                Search Tech Stacks
               </Nav.Link>
               {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
                 <>
                   <Nav.Link as={Link} to='/saved'>
-                    See Your Books
+                    See Your Teck Stacks
                   </Nav.Link>
                   <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                 </>
